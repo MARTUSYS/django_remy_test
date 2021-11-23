1be9410a89f948e561b9a89bde82cda504f579a7
MARTUSYS <jonk98@mail.ru>
2021-11-23 18:06:39 +1000

    исправление для секретного ключа

---
+++ remy/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+def start(request):
+    """
+    Отображение главной страницы
+    :param request:
+    :return:
+    """
+    return None
+
+
+def catalog(request):
+    """
+    Отображение каталога
+    :param request:
+    :return:
+    """
+    return
--- remy/remy/settings.py
+++ remy/remy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import configparser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=jq9(6g3y#982&(jpfmv5(%sz!r4lqp5sryzc)=gu8653vnsa2'
+key_config = configparser.RawConfigParser()
+key_config.read('../key.ini')
+SECRET_KEY = key_config['settings']['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,8 @@ WSGI_APPLICATION = 'remy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'db.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
